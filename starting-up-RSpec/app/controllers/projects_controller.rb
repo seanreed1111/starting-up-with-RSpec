44bd9f3d4661386e6e6b5ff15f9ec737fb8bc5ad
seanreed1111 <seanreed1111@gmail.com>
2016-06-02 15:03:44 -0400

    can now update projects

--- starting-up-RSpec/app/controllers/projects_controller.rb
+++ starting-up-RSpec/app/controllers/projects_controller.rb
@@ -4,15 +4,27 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find params[:id]
+    @project = Project.find(params[:id])
   end
 
   def new
     @project = Project.new
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+
+    flash[:notice] = "Project has been updated."
+    redirect_to @project
+  end
+
   def create
-    @project = Project.new project_params
+    @project = Project.new(project_params)
 
     if @project.save
       flash[:notice] = "Project has been created."
@@ -25,7 +37,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:name, :description)
+    params.require(:project).permit(:name, :description, :id)
   end
 
 end
