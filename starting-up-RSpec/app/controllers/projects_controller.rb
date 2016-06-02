0f04958bddad5996187cd8827ab3dc15bb1fd622
seanreed1111 <seanreed1111@gmail.com>
2016-06-02 15:47:15 -0400

    can now delete projects

--- starting-up-RSpec/app/controllers/projects_controller.rb
+++ starting-up-RSpec/app/controllers/projects_controller.rb
@@ -41,6 +41,14 @@ class ProjectsController < ApplicationController
 
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+
   def project_params
     params.require(:project).permit(:name, :description)
   end
