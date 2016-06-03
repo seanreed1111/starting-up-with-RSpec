7a7456d6f0eb06deb7ea480529d1357615086f0f
seanreed1111 <seanreed1111@gmail.com>
2016-06-03 14:39:34 -0400

    adds ability to create tickets for each project

--- starting-up-RSpec/db/schema.rb
+++ starting-up-RSpec/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601174843) do
+ActiveRecord::Schema.define(version: 20160603171203) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20160601174843) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "project_id"
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
 
 end
